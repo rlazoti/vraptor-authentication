19db3c5ce50f22effbe81f2877787db4b8ea977f
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2012-03-03 15:56:52 -0300

    Added: Annotations.

---
+++ src/main/java/br/com/rlazoti/vraptor/authentication/annotations/Public.java
@@ -0,0 +1,12 @@
+package br.com.rlazoti.vraptor.authentication.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Public {
+
+}
